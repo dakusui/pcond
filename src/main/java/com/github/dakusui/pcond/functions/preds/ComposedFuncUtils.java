f0a6071dc7d13407fcd96dcc460abfb6f42b7e09
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2020-04-22 17:24:15 +0900

    WIP: Refactor codes

--- src/main/java/com/github/dakusui/pcond/functions/preds/ComposedFuncUtils.java
+++ src/main/java/com/github/dakusui/pcond/functions/preds/ComposedFuncUtils.java
@@ -11,41 +11,42 @@ import static com.github.dakusui.pcond.internals.InternalUtils.toEvaluableIfNece
 public enum ComposedFuncUtils {
   ;
 
-  public static <T, R> BaseFuncUtils.Factory<T, R, List<Function<Object, Object>>> factory(
+  public static <T, R> Factory<T, R> factory(
       Function<List<Function<Object, Object>>, String> nameComposer, Function<List<Function<Object, Object>>, Function<T, R>> ff) {
-    return new BaseFuncUtils.Factory<T, R, List<Function<Object, Object>>>(nameComposer) {
+    return new ComposedFuncUtils.Factory<T, R>(nameComposer, ff) {
       public PrintableFunction<T, R> create(List<Function<Object, Object>> arg) {
         final Lambda.Spec<List<Function<Object, Object>>> spec = new Lambda.Spec<>(this, arg, PrintableFunctionFromFactory.class);
         final Function<? super T, ? extends R> function = createFunction(arg);
         return new PrintableFunctionFromFactory<T, R, List<Function<Object, Object>>>(
             () -> this.nameComposer().apply(arg), function, createHead(arg), createTail(arg)) {
-
           @Override
           public Spec<List<Function<Object, Object>>> spec() {
             return spec;
           }
         };
       }
-
-      @Override
-      public Function<T, R> createFunction(List<Function<Object, Object>> arg) {
-        return ff.apply(arg);
-      }
-
-      @SuppressWarnings("unchecked")
-      Function<? super T, ? extends R> createHead(List<Function<Object, Object>> arg) {
-        return (Function<? super T, ? extends R>) arg.get(0);
-      }
-
-      Evaluable<T> createTail(List<Function<Object, Object>> arg) {
-        return toEvaluableIfNecessary(arg.get(1));
-      }
     };
   }
 
-  public static abstract class Factory<T, R, E> extends BaseFuncUtils.Factory<T, R, E> {
-    protected Factory(Function<E, String> s) {
+  public static abstract class Factory<T, R> extends BaseFuncUtils.Factory<T, R, List<Function<Object, Object>>> {
+    private final Function<List<Function<Object, Object>>, Function<T, R>> ff;
+
+    protected Factory(Function<List<Function<Object, Object>>, String> s, Function<List<Function<Object, Object>>, Function<T, R>> ff) {
       super(s);
+      this.ff = ff;
+    }
+
+    public Function<T, R> createFunction(List<Function<Object, Object>> arg) {
+      return ff.apply(arg);
+    }
+
+    @SuppressWarnings("unchecked")
+    Function<? super T, ? extends R> createHead(List<Function<Object, Object>> arg) {
+      return (Function<? super T, ? extends R>) arg.get(0);
+    }
+
+    Evaluable<T> createTail(List<Function<Object, Object>> arg) {
+      return toEvaluableIfNecessary(arg.get(1));
     }
   }
 }
