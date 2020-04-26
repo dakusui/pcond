bcf0150f1fb0f885c159d192deea07aa79b01e1c
hiroshi.ukai <hiroshi.ukai@rakuten.com>
2020-04-26 21:15:57 +0900

    Improve quick start and readme.

--- src/test/java/com/github/dakusui/pcond/examples/Example.java
+++ src/test/java/com/github/dakusui/pcond/examples/Example.java
@@ -1,23 +1,27 @@
 package com.github.dakusui.pcond.examples;
 
-import static com.github.dakusui.pcond.Assertions.that;
-import static com.github.dakusui.pcond.Postconditions.ensureNonNull;
-import static com.github.dakusui.pcond.Preconditions.requireArgument;
+// <1>
+import static com.github.dakusui.pcond.Assertions.*;
+import static com.github.dakusui.pcond.Postconditions.*;
+import static com.github.dakusui.pcond.Preconditions.*;
+import static com.github.dakusui.pcond.functions.Functions.*;
 import static com.github.dakusui.pcond.functions.Predicates.*;
 
 public class Example {
-  public static String hello(String yourFirstName) {
-    requireArgument(yourFirstName, and(isNotNull(), not(isEmptyString()), not(containsString(" "))));
-
-    String format = "Hello, %s";
-    assert that(format, isNotNull());
-
-    String ret = String.format(format, yourFirstName);
+  public static String hello(String yourName) {
+    // <2>
+    requireArgument(yourName, and(isNotNull(), transform(length()).check(gt(0)), containsString(" ")));
+    String ret = String.format("Hello, %s", firstNameOf(yourName));
+    // <3>
     return ensureNonNull(ret);
   }
 
+  private static String firstNameOf(String yourName) {
+    assert precondition(yourName, containsString(" ")); // <4>
+    return yourName.substring(0, yourName.indexOf(' '));
+  }
+
   public static void main(String[] args) {
-    System.out.println(hello("John"));
-    System.out.println(hello("John Doe"));
+    System.out.println(hello("JohnDoe"));
   }
 }
