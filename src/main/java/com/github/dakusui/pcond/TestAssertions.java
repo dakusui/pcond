5cb17bff3ab25a6b2f6a7b13e060f9aed485aba9
Hiroshi Ukai <dakusui@gmail.com>
2022-05-28 10:13:01 +0900

    Remove premature improvement.

--- src/main/java/com/github/dakusui/pcond/TestAssertions.java
+++ src/main/java/com/github/dakusui/pcond/TestAssertions.java
@@ -15,18 +15,4 @@ public enum TestAssertions {
   public static <T> void assumeThat(T value, Predicate<? super T> predicate) {
     AssertionProvider.INSTANCE.assumeThat(value, predicate);
   }
-
-  public static <T> void assertThat(Statement<T> statement) {
-    assertThat(statement.value(), statement.predicate());
-  }
-
-  public static <T> void assumeThat(Statement<T> statement) {
-    assumeThat(statement.value(), statement.predicate());
-  }
-
-  interface Statement<T> {
-    T value();
-
-    Predicate<T> predicate();
-  }
 }
