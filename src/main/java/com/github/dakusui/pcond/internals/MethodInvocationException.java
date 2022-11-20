96b59cb315ea8ebd62acb7ff4a32880397512b4b
Hiroshi Ukai <dakusui@gmail.com>
2022-11-20 22:56:32 +0900

    Make MethodInvocationTargetException not an InternalException.

--- src/main/java/com/github/dakusui/pcond/internals/MethodInvocationException.java
+++ src/main/java/com/github/dakusui/pcond/internals/MethodInvocationException.java
@@ -1,6 +1,6 @@
 package com.github.dakusui.pcond.internals;
 
-public class MethodInvocationException extends InternalException {
+public class MethodInvocationException extends RuntimeException {
   public MethodInvocationException(String message, Throwable cause) {
     super(message, cause);
   }
